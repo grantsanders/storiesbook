495340cc01fa6693d717c0c10a086d53b0100743
grantsanders <grantsanders89@gmail.com>
2024-09-24 18:11:16 -0400

    added environment variables for use in GCP

--- storiesbook/Program.cs
+++ storiesbook/Program.cs
@@ -7,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Configuration.AddEnvironmentVariables();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<JiraService>();
