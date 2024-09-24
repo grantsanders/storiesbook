67265410ccc439825f798309b20d97ae8be58d3c
grantsanders <gsanders@ecs-online.com>
2024-09-24 16:16:39 -0400

    added GetTicketInformation method to JiraService.cs

--- storiesbook/Services/JiraService.cs
+++ storiesbook/Services/JiraService.cs
@@ -1,9 +1,32 @@
+using Atlassian.Jira;
+using Newtonsoft.Json;
+
 namespace storiesbook.Services;
 
 public class JiraService
 {
-    public JiraService()
+    private readonly IConfiguration _config;
+
+    public JiraService(IConfiguration config)
     {
+        _config = config;
+    }
+
+    public async Task<List<string>> GetTicketInformation(string ticketId)
+    {
+        var jira = Jira.CreateRestClient(_config["JIRA_URL"], _config["JIRA_USERNAME"], _config["JIRA_API_KEY"]);
+
+        var issue = await jira.Issues.GetIssueAsync(ticketId);
+        var comments = await issue.GetCommentsAsync();
+
+        var ticketInformation = new List<string>();
         
+        ticketInformation.Add(issue.Summary);
+        foreach (var comment in comments)
+        {
+            ticketInformation.Add(comment.Body);
+        }
+
+        return ticketInformation;
     }
 }
