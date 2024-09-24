42b23c094f018cbd128d4e5f94980f95b16899e7
grantsanders <gsanders@ecs-online.com>
2024-09-24 12:17:49 -0400

    removed base template information

--- storiesbook/Program.cs
+++ storiesbook/Program.cs
@@ -16,29 +16,15 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapPost("/SummarizeTicket", (string ticketId) =>
+    {
+        /* logic will go here:
+         • grab jira ticket info
+         • pass that info to ai summarization tool
+         • pass summarized text back to github
+         */
+    })
+    .WithName("SummarizeTicket")
+    .WithOpenApi();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
